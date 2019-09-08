9a96318508c60eb8de87bf9900c918bca17596e6
Christopher Iliyas <christopher.iliyas@andela.com>
2019-09-08 21:32:47 +0100

    add navigation and routes

--- basic-authentication/src/App.js
+++ basic-authentication/src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import LandingPage from './components/Landing';
+import SignUpPage from './components/SignUp';
+import SignInPage from './components/SignIn';
+import PasswordForgetPage from './components/PasswordForget';
+import HomePage from './components/Home';
+import AccountPage from './components/Account';
+import AdminPage from './components/Admin';
+import { routes } from './constants/routes';
 
-const App = () => {
-  return (
-    <div className="App">
-      Test
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+      <hr />
+      <Switch>
+        <Route exact path={routes.LANDING} component={LandingPage} />
+        <Route path={routes.SIGN_UP} component={SignUpPage} />
+        <Route path={routes.SIGN_IN} component={SignInPage} />
+        <Route path={routes.PASSWORD_FORGET} component={PasswordForgetPage} />
+        <Route path={routes.HOME} component={HomePage} />
+        <Route path={routes.ACCOUNT} component={AccountPage} />
+        <Route path={routes.ADMIN} component={AdminPage} />
+      </Switch>
     </div>
+  </Router>
   );
-};
 
 export default App;
---
+++ basic-authentication/src/constants/routes.js
@@ -0,0 +1,9 @@
+export const routes = Object.freeze({
+    LANDING: '/',
+    SIGN_UP: '/signup',
+    SIGN_IN: 'signin',
+    HOME: '/home',
+    ACCOUNT: '/account',
+    ADMIN: '/admin',
+    PASSWORD_FORGET: '/resetpassword'
+});
